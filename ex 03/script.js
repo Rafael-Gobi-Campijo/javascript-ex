fc93a7c2371fdcef3b439ef02403188a4749b540
Rafael Gobi Campijo <rafaelcampijo@gmail.com>
2026-03-03 10:09:42 -0300

    .

--- ex 03/script.js
+++
@@ -1,9 +0,0 @@
-var span = window.document.getElementById('span')
-
-var vitorias = prompt('Quantas vezes seu time ganhou?')
-var empates = prompt('Quantas vezes seu time empatou?')
-var derrotas = prompt('Quantas vezes seu time perdeu?')
-
-var pontuacao = (vitorias * 3) + (empates * 1)
-
-alert(`A pontuação do seu time foi ${pontuacao} pontos.`)
